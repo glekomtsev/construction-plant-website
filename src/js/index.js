504929645854a8a4fd340ae58c85e6e64195de28
Gleb Lekomtsev <lekomcevg@gmail.com>
2024-09-16 17:00:20 +0500

    add 1 and 2 slide

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,27 @@
 // Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// import { Fancybox } from '@fancyapps/ui';
+// import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
-});
+// Fancybox.bind('[data-fancybox]', {
+// 	// Your custom options
+// });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
+
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+const swiper = new Swiper(".swiper-main", {
+  // Optional parameters
+  direction: "vertical",
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
